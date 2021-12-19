caa213acda4abffa940b044047d75888d809e511
Matthew Greco <mgreco2112@tutanota.com>
2021-12-19 10:08:36 -0500

    added comments for learning clarity

--- src/components/common/CounterCard.js
+++ src/components/common/CounterCard.js
@@ -3,10 +3,13 @@ import Counter from "../counter/Counter";
 
 const CounterCard = (props) => {
     // const count = 2;
+
+    //Object Destructuring of count, setCount Array? Will ask on Monday
     const [count, setCount] = useState(0)
 
     return (
         <div
+            //HTML Style for Div
             style={
                 {
                     border: 'lpx solid #171717',
@@ -19,8 +22,10 @@ const CounterCard = (props) => {
                     boxShadow: '1px 1px 20px rgba(91, 91, 91, 0.5)'
                 }
             }
+            //HTML OnClick Anon Function for Div
             onClick={() => setCount(count + 1)}
         >
+            {/* InnerText Div in the psudo HTML context, imported from Counter file*/}
             <Counter count={count}/>
         </div>
     )
